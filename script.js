177bed1dab0be3883e310e48b64498febff4be14
SIMPARA <simp73337988@gmail.com>
2025-04-09 17:32:18 +0000

    mon_second_commit

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-// // Function to download the CV as a PDF
-// function telechargerCV() {
-//     // Sélectionnez la partie du CV à convertir (évite d'inclure le bouton de téléchargement)
-//     const element = document.getElementById('cv');
+// Function to download the CV as a PDF
+function telechargerCV() {
+    // Sélectionnez la partie du CV à convertir (évite d'inclure le bouton de téléchargement)
+    const element = document.getElementById('cv');
     
-//     // Options du PDF
-//     const opt = {
-//         margin: [5, 5, 5, 5], 
-//         filename: 'CV_Cheickna_Hamala.pdf',
-//         image: { type: 'jpeg', quality: 0.98 },
-//         html2canvas: { 
-//             scale: 1, // Améliore la qualité
-//             useCORS: true, // Permet le chargement des images externes
-//             logging: true, // Désactive les lo gs pour plus de rapidité
-//             scrollX:0,
-//             scrollY:50,
-//         },
-//         jsPDF: { 
-//             unit: 'mm', 
-//             format: 'a4', 
-//             // orientation: 'portrait' 
-//         }
-//     };
+    // Options du PDF
+    const opt = {
+        margin: [5, 5, 5, 5], 
+        filename: 'CV_Cheickna_Hamala.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { 
+            scale: 1, // Améliore la qualité
+            useCORS: true, // Permet le chargement des images externes
+            logging: true, // Désactive les lo gs pour plus de rapidité
+            scrollX:0,
+            scrollY:0,
+            
+        },
+        jsPDF: { 
+            unit: 'mm', 
+            format: 'a4', 
+            // orientation: 'portrait' 
+        }
+    };
 
-//     // Générer et télécharger le PDF
-//     html2pdf().set(opt).from(element).save();
-// }
+    // Générer et télécharger le PDF
+    html2pdf().set(opt).from(element).save();
+}
 
-let mon_btn =
-document.getElementById('mon_btn');
-mon_btn.addEventListener('click',function () {
+function retourAccueil() {
+    window.location.href = "index.html"; // ou "/" si ton fichier est à la racine
+}
+
+// let mon_btn =
+// document.getElementById('mon_btn');
+// mon_btn.addEventListener('click',function () {
     
-    let simp = 
-    document.getElementById('cv')
-    html2pdf().from(simp).save('mon_cv.pdf')
-})
+//     let simp = 
+//     document.getElementById('cv')
+//     html2pdf().from(simp).save('mon_cv.pdf')
+// })
 
 
 // change coulaure
